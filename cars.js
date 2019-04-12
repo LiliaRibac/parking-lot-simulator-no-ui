e523f42d0b2a8e6cafe8e7f2e222b19c74af2925
LiliaRibac <ribac.lilia@gmai.com>
2019-04-12 16:57:05 -0500

    add js

--- cars.js
+++ cars.js
@@ -1,34 +1,37 @@
 "use stric"
 
-const NUMBER_OF_CARS = 5;
-const NUMBER_OF_SPACES = 1;
+const NUMBER_OF_CARS = 100;
+const NUMBER_OF_SPACES = 10;
 
 
 var qWait = [];
-
 var parkingSpaces = [];
-// const freeToGo = [];
+var freeToGo = [];
 
-var spaceOpen = NUMBER_OF_SPACES - parkingSpaces.length;
+function spaceOpen() {
+
+    return (NUMBER_OF_SPACES - parkingSpaces.length);
+}
 
 // var license = lisenceFactory()
 
-function yearRandom(e) {
-    var e = Math.floor(Math.random() * 15) + 2020;
+function yearRandom() {
+    var e = Math.floor(Math.random() * 6) + 2014;
     return e;
 }
 
-function timeRandom(t) {
-    var t = Math.floor(Math.random() * ( 4000 - 1000) + 1000);
+function timeRandom() {
+    var t = Math.floor(Math.random() * 4000) + 1000;
     return t;
 }
 
 function lisenceFactory(lengthde) {
-    var l = ""
-    var alfabet = 'ABCDEFGHLJKQWERLTYUIOPMNVCZ0123456789';
+    var l = "";
+    var alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
     while (l.length < 5) {
         l += alfabet[Math.floor(Math.random() * alfabet.length)];
+
     }
     return l;
 
@@ -44,35 +47,47 @@ function randomColor() {
     return arrColor[Math.floor(Math.random() * arrColor.length)];
 }
 
-function Car(model, color, lisencePlate, year, time) {
+function Car() {
     this.model = randomModel();
     this.color = randomColor();
     this.lisencePlate = lisenceFactory();
     this.year = yearRandom();
-    this.time = timeRandom()
-
-    this.park = function (car) {
-        console.log("park", car);
-        parkingSpaces.push(car)
+    this.time = timeRandom();
 
 
-        setTimeout(function(){car.leave(car)}, this.time);
+
+    this.park = function () {
+
+        parkingSpaces.push(this);
+
+
+        setTimeout(function (e) {
+            e.leave();
+        }, this.time, this);
+
+        //   console.log(parkingSpaces)
     }
 
-    this.leave = function (car) {
-        console.log("Leave!", car)
-        parkingSpaces.pop(car);
-
-        Attendant()
+    this.leave = function () {
+        let plateNumbers = parkingSpaces.map(function(x) {
+            return x.lisencePlate; 
+        })
+        
+        let position =plateNumbers.indexOf(this.lisencePlate);
+        let car = parkingSpaces.splice(position,1);
+        freeToGo.push(car);
+        console.table(this);
+        console.count("leave()");
+        Attendant();
     }
 }
 
 function allCarsRandom() {
     for (i = 0; i < NUMBER_OF_CARS; i++) {
         let car = new Car();
-        qWait.push(car)
+        qWait.push(car);
     }
-    console.log(qWait)
+    // console.log(qWait)
     return qWait;
 }
 
@@ -81,39 +96,37 @@ function allCarsRandom() {
 
 function Attendant() {
     for (let i = 0; i < NUMBER_OF_SPACES; i++) {
-        if (spaceOpen !== 0 && qWait.length !== 0) {
-            var car = qWait.pop()
+        if (spaceOpen() !== 0 && qWait.length !== 0) {
+            var car = qWait.pop();
 
-            car.park(car)
-            
+            car.park(car);
+
         } else {
-            console.log('No more spaces available!')
+
+            // console.log('No more spaces available!')
         }
     }
 
 }
-
-// var timeoutId = setTimeout(Car, 1000);
-// clearTimeout(timeoutId);
-
-// var intervalId = setInterval(allCarsRandom, 1000)
-// clearInterval(intervalId);
-
-// let timer = setTimeout(function myTimer() {
-//     increment();
-   
-
-//     timer = setTimeout(myTimer, 1000);
-// }, 1000);
-
-
-// let's cancel after 7 seconds
-// setTimeout(() => {
-//     console.log('Cancelling');
-//     clearTimeout(timer);
-// }, 10000);
-
+function allCarsGone(){
+    let  y = qWait.length;
+    let  z = parkingSpaces.length;
+    if(z === 0 && y === 0){
+        clearInterval(intervalId);
+    }
+}
 
 allCarsRandom();
-Attendant()
-console.log(qWait);
+Attendant();
+let intervalId = setInterval(function() {
+    console.clear();
+    console.log(`Queued: ${qWait.length}`);
+    console.log(`Parked: ${parkingSpaces.length}`);
+    console.log(`Departed: ${freeToGo.length}`);
+    console.log(qWait);
+    console.log(parkingSpaces);
+    console.log(freeToGo);
+    allCarsGone();
+    
+}, 1450);
+//  console.log(qWait);
